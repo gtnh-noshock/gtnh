ccf32d5d1121aa68c844a33cd56a463081d44a3d
4o4E <869951226@qq.com>
2024-01-19 23:31:17 +0800

    update: backup.main.kts

--- kts/backup.main.kts
+++ kts/backup.main.kts
@@ -1,3 +1,9 @@
+@file:DependsOn("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import java.io.File
 import java.time.Duration
 import java.time.LocalDateTime
@@ -13,39 +19,54 @@ val backups: Array<File> = backupDir.listFiles() ?: exitProcess(0)
 val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm-ss")
 val printFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss")
 
-while (true) {
-    val now: LocalDateTime = LocalDateTime.now()
-    println("开始检查: ${now.format(printFormatter)}")
-    val preHour = mutableMapOf<String, String>()
-    backups.filter {
-        it.isDirectory
-    }.map {
-        it to LocalDateTime.parse(it.name, formatter)
-    }.sortedBy {
-        it.second.toEpochSecond(ZoneOffset.of("+8"))
-    }.forEach { (file, dateTime) ->
-        val hoursDiff = Duration.between(dateTime, now).toHours()
-        // 保留最近 3 小时内的备份
-        if (hoursDiff <= 3) return@forEach
+runBlocking(Dispatchers.IO) {
+    while (true) {
+        launch {
+            println("服务器存档备份")
+            Runtime.getRuntime().exec("tmux send-keys -t gtnh:0 save-all C-m").waitFor()
+            Thread.sleep(3000)
+            val dirName = formatter.format(LocalDateTime.now())
+            File(dirName).mkdir()
+            val fileName = "$dirName/backup.zip"
+            println("开始备份: $fileName")
+            Runtime.getRuntime().exec("zip -qr $fileName ../World").waitFor()
+            println("完成备份: $fileName")
 
-        // 保留 3小时-7天 每小时的第一个备份
-        if (hoursDiff <= 168) {
-            val hourKey = "${dateTime.year}-${dateTime.monthValue}-${dateTime.dayOfMonth}-${dateTime.hour}"
-            // 保留每小时的第一份
-            val exists = preHour[hourKey]
-            if (exists == null) {
-                preHour[hourKey] = dateTime.format(printFormatter)
-                return@forEach
+            val now: LocalDateTime = LocalDateTime.now()
+            println("开始检查过期备份")
+            val preHour = mutableMapOf<String, String>()
+            backups.filter {
+                it.isDirectory
+            }.map {
+                it to LocalDateTime.parse(it.name, formatter)
+            }.sortedBy {
+                it.second.toEpochSecond(ZoneOffset.of("+8"))
+            }.forEach { (file, dateTime) ->
+                val hoursDiff = Duration.between(dateTime, now).toHours()
+                // 保留最近 3 小时内的备份
+                if (hoursDiff <= 3) return@forEach
+
+                // 保留 3小时-7天 每小时的第一个备份
+                if (hoursDiff <= 168) {
+                    val hourKey = "${dateTime.year}-${dateTime.monthValue}-${dateTime.dayOfMonth}-${dateTime.hour}"
+                    // 保留每小时的第一份
+                    val exists = preHour[hourKey]
+                    if (exists == null) {
+                        preHour[hourKey] = dateTime.format(printFormatter)
+                        return@forEach
+                    }
+                    file.deleteRecursively()
+                    println("删除${dateTime.format(printFormatter)}: 已有当前小时的备份${exists}")
+                    return@forEach
+                }
+
+                // 删除7天以前的备份
+                file.deleteRecursively()
+                println("删除${dateTime.format(printFormatter)}: 7天前")
             }
-            file.deleteRecursively()
-            println("删除${dateTime.format(printFormatter)}: 已有当前小时的备份${exists}")
-            return@forEach
+            println("完成检查过期备份")
+            println()
         }
-
-        // 删除7天以前的备份
-        file.deleteRecursively()
-        println("删除${dateTime.format(printFormatter)}: 7天前")
+        delay(5 * 60 * 1000)
     }
-
-    Thread.sleep(10 * 60 * 1000)
 }
