c2a338dfe09c4c401df5eab3db49799e38272f8b
4o4E <869951226@qq.com>
2024-02-16 02:04:02 +0800

    update: 备份脚本添加广播消息

--- kts/backup.main.kts
+++ kts/backup.main.kts
@@ -29,12 +29,19 @@ runBlocking(Dispatchers.IO) {
             File(dirName).mkdir()
             val fileName = "$dirName/backup.zip"
             println("开始备份: $fileName")
-            Runtime.getRuntime().exec("zip -qr $fileName ../World").waitFor()
-            println("完成备份: $fileName")
+            launch {
+                Runtime.getRuntime().exec(arrayOf("tmux", "send-keys", "-t", "gtnh:0", "say 开始备份: $fileName", "C-m")).waitFor()
+            }
+            launch {
+                Runtime.getRuntime().exec("zip -qr $fileName ../World").waitFor()
+                println("完成备份: $fileName")
+                Runtime.getRuntime().exec(arrayOf("tmux", "send-keys", "-t", "gtnh:0", "say 完成备份: $fileName", "C-m")).waitFor()
+            }
 
             val now: LocalDateTime = LocalDateTime.now()
             println("开始检查过期备份")
             val preHour = mutableMapOf<String, String>()
+            var delete = 0
             backups.filter {
                 it.isDirectory
             }.map {
@@ -56,15 +63,20 @@ runBlocking(Dispatchers.IO) {
                         return@forEach
                     }
                     file.deleteRecursively()
+                    delete++
                     println("删除${dateTime.format(printFormatter)}: 已有当前小时的备份${exists}")
                     return@forEach
                 }
 
                 // 删除7天以前的备份
                 file.deleteRecursively()
+                delete++
                 println("删除${dateTime.format(printFormatter)}: 7天前")
             }
-            println("完成检查过期备份")
+            launch {
+                Runtime.getRuntime().exec(arrayOf("tmux", "send-keys", "-t", "gtnh:0", "say 完成检查过期备份 共删除$delete", "C-m")).waitFor()
+            }
+            println("完成检查过期备份 共删除$delete")
             println()
         }
         delay(5 * 60 * 1000)
